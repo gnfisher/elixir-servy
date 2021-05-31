9ccf0d263529248945bd152f63f16574982e36f2
Greg Fisher <gnfisher@thoughtbot.com>
2021-05-30 21:21:21 -0400

    mmax

--- lib/progamming_elixir/my_list.ex
+++ lib/progamming_elixir/my_list.ex
@@ -1,16 +1,23 @@
 defmodule ProgrammingElixir.MyList do
   def mapsum([], _func), do: 0
 
-  def mapsum([head | _tail], _func) when not is_integer(head) do
-    raise "Not an integer"
-  end
-
-  def mapsum([head | tail], func) do
+  def mapsum([head | tail], func) when is_integer(head) do
     func.(head) + mapsum(tail, func)
   end
 
-  def max([]), do: raise "Cannot find max of empty list"
-  def max([head | tail]) when not is_integer(head) do
-    raise "Cannot find max of non integer"
+  def mmax([]), do: raise "Cannot find max of empty list"
+
+  def mmax([head | tail]) when is_integer(head) do
+    mmax(tail, head)
+  end
+
+  defp mmax([], value), do: value
+
+  defp mmax([head | tail], value) when is_integer(head) and head > value do
+    mmax(tail, head)
+  end
+
+  defp mmax([head | tail], value) when is_integer(head) and head <= value do
+    mmax(tail, value)
   end
 end
